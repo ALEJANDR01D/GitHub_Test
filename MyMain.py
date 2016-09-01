b3f3f6ca72311d0f8c666ddaa2dc31771686840d
alejandro <aarauz15@gmail.com>
2016-09-01 14:43:11 -0500

    myMain changes

--- MyMain.py
+++ MyMain.py
@@ -1,1 +1,14 @@
-print ("hello!")
+import unittest
+from teamcity import is_running_under_teamcity
+from teamcity.unittestpy import TeamcityTestRunner
+
+class Test(unittest.TestCase):
+
+if __name__ == '__main__':
+    if is_running_under_teamcity():
+	    print ("IF: teamcityTest runner")
+        runner = TeamcityTestRunner()
+    else:
+        print ("ELSE: unittest runner")
+		runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
