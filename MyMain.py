0a31b65ab8a084dbadb90872ff6e567aee5d6883
alejandro <aarauz15@gmail.com>
2016-09-02 14:26:44 -0500

    changes 2

--- MyMain.py
+++ MyMain.py
@@ -1,14 +1,33 @@
-import unittest
+# coding=utf-8
 from teamcity import is_running_under_teamcity
 from teamcity.unittestpy import TeamcityTestRunner
 
-class Test(unittest.TestCase):
+import unittest
+
+
+class TestTeamcityMessages(unittest.TestCase):
+    def testPass(self):
+        assert 1 == 1
+
+    def testAssertEqual(self):
+        self.assertEqual("1", "2")
+
+    def testAssertEqualMsg(self):
+        self.assertEqual("1", "2", "message")
+
+    def testAssert(self):
+        self.assert_(False)
+
+    def testFail(self):
+        self.fail("failed")
+
+    def testException(self):
+        raise Exception("some exception")
+
 
 if __name__ == '__main__':
     if is_running_under_teamcity():
-	    print ("IF: teamcityTest runner")
         runner = TeamcityTestRunner()
     else:
-        print ("ELSE: unittest runner")
-		runner = unittest.TextTestRunner()
+        runner = unittest.TextTestRunner()
     unittest.main(testRunner=runner)
