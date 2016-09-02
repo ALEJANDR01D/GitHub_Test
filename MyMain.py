00df4d73bfbf26bb33d09c09edf599849fa42d22
alejandro <aarauz15@gmail.com>
2016-09-02 14:36:31 -0500

    unittest lib Teamcity

--- MyMain.py
+++ MyMain.py
@@ -9,11 +9,17 @@ class TestTeamcityMessages(unittest.TestCase):
     def testPass(self):
         assert 1 == 1
 
-    def testAssertEqual(self):
+	def testAssertEqualTrue(self):
+        self.assertEqual("1", "1")
+		
+    def testAssertEqualFalse(self):
         self.assertEqual("1", "2")
 
-    def testAssertEqualMsg(self):
-        self.assertEqual("1", "2", "message")
+    def testAssertEqualTrueMsg(self):
+        self.assertEqual("1", "1", "Both numbers equals!")
+		
+    def testAssertEqualFalseMsg(self):
+        self.assertEqual("1", "2", "Numbers are not equal!")
 
     def testAssert(self):
         self.assert_(False)
@@ -22,12 +28,14 @@ class TestTeamcityMessages(unittest.TestCase):
         self.fail("failed")
 
     def testException(self):
-        raise Exception("some exception")
+        raise Exception("My test exception message!")
 
 
 if __name__ == '__main__':
     if is_running_under_teamcity():
+		print ("TeamCityTestRunner")
         runner = TeamcityTestRunner()
     else:
+		print ("unittest.TextTestRunner")
         runner = unittest.TextTestRunner()
     unittest.main(testRunner=runner)
